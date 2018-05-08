4c88465d91631d0b667b43ecc126b9a26bcf0858
Majak <majakthecoder@gmail.com>
2018-05-07 22:11:24 -0700

    database

--- data/movies_db.php
+++ data/movies_db.php
@@ -4,46 +4,46 @@ $movies_db_json = '
 {
 	"tv_shows": [
 		{
-			"featured": "TRUE",
-			"season": "8",
+			"featured": true,
+			"season": 8,
 			"imdb_url": "https://www.imdb.com/title/tt0944947/",
 			"title": "Game of Thrones",
 			"watch_logo": "hbo",
-			"imdb_score": "9.5",
+			"imdb_score": 9.5,
 			"watch_url": "https://www.hbo.com/game-of-thrones?pid=googleadwords_int&c=Google|Search|MKL|IQ_ID_105713171-VQ16-c&camp=Google|Search|MKL|IQ_ID_105713171-VQ16-c",
-			"day": "1",
+			"day": 1,
 			"source_url": "http://www.digitalspy.com/tv/game-of-thrones/feature/a834284/game-of-thrones-season-8-release-date-trailer-episodes-spoiler-filming/",
-			"year": "2018",
+			"year": 2018,
 			"thumbnail": "game_of_thrones.jpg",
-			"month": "10"
+			"month": 10
 		},
 		{
-			"featured": "FALSE",
-			"season": "4",
+			"featured": false,
+			"season": 4,
 			"imdb_url": "https://www.imdb.com/title/tt4158110/",
 			"title": "Mr. Robot",
 			"watch_logo": "amazon",
-			"imdb_score": "8.6",
+			"imdb_score": 8.6,
 			"watch_url": "https://www.amazon.com/dp/B0195ITNBY?ref_=imdbref_tt_wbr_piv&tag=imdbtag_tt_wbr_piv-20",
-			"day": "2",
+			"day": 2,
 			"source_url": "http://www.digitalspy.com/tv/mr-robot/feature/a845419/mr-robot-season-4-release-date-cast-trailer-plot/",
-			"year": "2019",
+			"year": 2019,
 			"thumbnail": "mr_robot.jpg",
-			"month": "11"
+			"month": 11
 		},
 		{
-			"featured": "FALSE",
-			"season": "2",
+			"featured": false,
+			"season": 2,
 			"imdb_url": "https://www.imdb.com/title/tt0475784/",
 			"title": "Westworld",
 			"watch_logo": "hbo",
-			"imdb_score": "8.9",
+			"imdb_score": 8.9,
 			"watch_url": "https://www.hbo.com/westworld/season-2?pid=googleadwords_int&c=Google|Search|MKL|IQ_ID_105612183-VQ16-c&camp=Google|Search|MKL|IQ_ID_105612183-VQ16-c",
-			"day": "3",
+			"day": 3,
 			"source_url": "http://www.digitalspy.com/tv/westworld/feature/a818341/westworld-season-2-2018-renewal-release-date-cast-filming-theories-spoilers/",
-			"year": "2020",
+			"year": 2020,
 			"thumbnail": "underworld.jpg",
-			"month": "12"
+			"month": 12
 		}
 	]
 }
