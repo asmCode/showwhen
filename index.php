bbf2294bada03bc76d5706328b40e7c52673c087
Majak <majakthecoder@gmail.com>
2018-10-15 00:12:26 -0700

    proper initialization of GA

--- index.php
+++ index.php
@@ -512,6 +512,16 @@ function GetDateAsString($tv_show)
 	function gtag(){dataLayer.push(arguments);}
 	gtag('js', new Date());
 	gtag('config', 'UA-118904948-1');
+
+	// JavaScript tracking snippet
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-118904948-1', 'auto');
+	// I think I don't need it
+	// ga('send', 'pageview');
 	</script>
 
 	<meta charset="UTF-8">
